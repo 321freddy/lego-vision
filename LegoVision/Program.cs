ceaeefeebb95d0de493ec1990bd65205861bae0a
321freddy <fred.ru.7@gmail.com>
2020-01-28 23:09:54 +0100

    Fixed python error. Added working test neural net

--- LegoVision/Program.cs
+++ LegoVision/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Keras;
+using Keras.Layers;
+using Keras.Models;
+using Numpy;
+using System;
+using System.IO;
 
 namespace LegoVision
 {
@@ -7,6 +12,37 @@ namespace LegoVision
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Load train data
+            NDarray x = np.array(new float[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } });
+            NDarray y = np.array(new float[] { 0, 1, 1, 0 });
+
+            //Build sequential model
+            var model = new Sequential();
+            model.Add(new Dense(32, activation: "relu", input_shape: new Shape(2)));
+            model.Add(new Dense(64, activation: "relu"));
+            model.Add(new Dense(1, activation: "sigmoid"));
+
+            //Compile and train
+            model.Compile(optimizer: "sgd", loss: "binary_crossentropy", metrics: new string[] { "accuracy" });
+            model.Fit(x, y, batch_size: 2, epochs: 1000, verbose: 1);
+
+            ////Save model and weights
+            //string json = model.ToJson();
+            //File.WriteAllText("model.json", json);
+            //model.SaveWeight("model.h5");
+
+            ////Load model and weight
+            //var loaded_model = Sequential.ModelFromJson(File.ReadAllText("model.json"));
+            //loaded_model.LoadWeight("model.h5");
+
+
+            //Load train data
+            NDarray testInput = np.array(new float[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } });
+
+            Console.WriteLine(model.Predict(testInput).ToString());
+
+            Console.ReadKey();
         }
     }
 }
