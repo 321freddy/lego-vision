87fdcf72e269009b397b6c40fcd2147806add0f0
KnowsNothing1 <49269176+KnowsNothing1@users.noreply.github.com>
2020-02-28 15:00:21 +0100

    Schnellerer Greyscale Algorithmus

--- TrainDataCreator/ImageProcessing.cs
+++ TrainDataCreator/ImageProcessing.cs
@@ -8,6 +8,8 @@ using ImageProcessor;
 using ImageProcessor.Imaging.Filters.EdgeDetection;
 using ImageProcessor.Imaging.Filters.Photo;
 using System.Drawing;
+using System.Drawing.Imaging;
+
 
 
 
@@ -44,7 +46,7 @@ namespace TrainDataCreator
                 Image original = Image.FromFile((filePaths[i]));
                 Bitmap resized = new Bitmap(original, new Size(aimWidth, aimHeight));
 
-                Bitmap greyscale = Makegreyscale(resized);
+                Bitmap greyscale = MakeGrayscale3(resized);
                 greyscale.Save(aimDir + "/res" + i + ".png", System.Drawing.Imaging.ImageFormat.Png);
                 /*imageProcessor.Load(filePaths[i]);
                 imageProcessor.Filter(filter);
@@ -61,7 +63,7 @@ namespace TrainDataCreator
             filePaths = Directory.GetFiles(path);
         }
 
-        public Bitmap Makegreyscale(Bitmap original)
+        public Bitmap MakeGrayscale(Bitmap original)
         {
             
             //make an empty bitmap the same size as original
@@ -88,7 +90,40 @@ namespace TrainDataCreator
 
             return newBitmap;
         }
+        public static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
 
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+         new float[] {.3f, .3f, .3f, 0, 0},
+         new float[] {.59f, .59f, .59f, 0, 0},
+         new float[] {.11f, .11f, .11f, 0, 0},
+         new float[] {0, 0, 0, 1, 0},
+         new float[] {0, 0, 0, 0, 1}
+               });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
 
     }
 
